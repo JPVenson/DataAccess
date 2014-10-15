2c93895398c7762119429648237fca6684448860
Jean-Pierre Bachmann <JPVenson@users.noreply.github.com>
2014-10-15 17:29:42 +0200

    Bug fixing and adding StackTrace to QueryDebugger

--- JPB.DataAccess/DebuggerHelper/QueryDebugger.cs
+++ JPB.DataAccess/DebuggerHelper/QueryDebugger.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,14 +12,45 @@ namespace JPB.DataAccess.DebuggerHelper
     [DebuggerDisplay("Query : {DebuggerQuery}", Name = "Query")]
     public class QueryDebugger
     {
+        static QueryDebugger()
+        {
+            Assembly = Assembly.GetAssembly(typeof(QueryDebugger));
+        }
+
+        static readonly Assembly Assembly;
+
         public string DebuggerQuery { get; private set; }
         public string SqlQuery { get; private set; }
-        
+        public string StackTracer { get; private set; }
+
         public const string StartValuePart = @" {{";
         public const string EndValuePart = @"}} ";
-        
+
         public QueryDebugger(IDbCommand command)
         {
+            var frames = new StackTrace().GetFrames();
+            IEnumerable<StackFrame> stackFrames;
+            if (frames != null)
+            {
+                stackFrames = frames.Where(s =>
+                {
+                    var methodBase = s.GetMethod();
+                    if (Assembly.DefinedTypes.Contains(methodBase.DeclaringType))
+                        return false;
+
+                    if (methodBase.DeclaringType != null && methodBase.DeclaringType.Assembly.GlobalAssemblyCache)
+                        return false;
+
+                    return true;
+                });
+            }
+            else
+            {
+                stackFrames = new List<StackFrame>();
+            }
+            var enumerable = stackFrames.Select(s => s.ToString()).ToArray();
+            if(enumerable.Any())
+                StackTracer = enumerable.Aggregate((e, f) => e + Environment.NewLine + f);
             var debugquery = new StringBuilder(command.CommandText);
             var sqlReady = new StringBuilder(command.CommandText);
 
--- JPB.DataAccess/Manager/DbAccessLayerEvents.cs
+++ JPB.DataAccess/Manager/DbAccessLayerEvents.cs
@@ -17,6 +17,14 @@ namespace JPB.DataAccess.Manager
         public static bool RaiseStaticEvents { get; set; }
 
         public static event DatabaseActionHandler OnUnknownDelete;
+        public event DatabaseActionHandler OnDelete;
+        public static event DatabaseActionHandler OnUnknownSelect;
+        public event DatabaseActionHandler OnKnownSelect;
+        public static event DatabaseActionHandler OnUnknownUpdate;
+        public event DatabaseActionHandler OnKnownUpdate;
+        public static event DatabaseActionHandler OnUnknownInsert;
+        public event DatabaseActionHandler OnKnownUInsert;
+
         protected static void RaiseUnknownDelete(IDbCommand query)
         {
             if (!RaiseStaticEvents)
@@ -27,9 +35,9 @@ namespace JPB.DataAccess.Manager
                 handler.BeginInvoke(null, new DatabaseActionEvent(new QueryDebugger(query)), s => { }, null);
         }
 
-        public event DatabaseActionHandler OnDelete;
         protected virtual void RaiseKnownDelete(IDbCommand query)
         {
+            RaiseUnknownDelete(query);
             if (!RaiseEvents)
                 return;
             //Async invoke
@@ -38,7 +46,6 @@ namespace JPB.DataAccess.Manager
                 handler.BeginInvoke(this, new DatabaseActionEvent(new QueryDebugger(query)), s => { }, null);
         }
 
-        public static event DatabaseActionHandler OnUnknownSelect;
         protected static void RaiseUnknownSelect(IDbCommand query)
         {
             if (!RaiseStaticEvents)
@@ -48,9 +55,9 @@ namespace JPB.DataAccess.Manager
                 handler.BeginInvoke(null, new DatabaseActionEvent(new QueryDebugger(query)), s => { }, null);
         }
 
-        public event DatabaseActionHandler OnKnownSelect;
         protected virtual void RaiseKnownSelect(IDbCommand query)
         {
+            RaiseUnknownSelect(query);
             if (!RaiseEvents)
                 return;
             //Async invoke
@@ -59,7 +66,6 @@ namespace JPB.DataAccess.Manager
                 handler.BeginInvoke(this, new DatabaseActionEvent(new QueryDebugger(query)), s => { }, null);
         }
 
-        public static event DatabaseActionHandler OnUnknownUpdate;
         protected static void RaiseUnknwonUpdate(IDbCommand query)
         {
             if (!RaiseStaticEvents)
@@ -70,9 +76,9 @@ namespace JPB.DataAccess.Manager
                 handler.BeginInvoke(null, new DatabaseActionEvent(new QueryDebugger(query)), s => { }, null);
         }
 
-        public event DatabaseActionHandler OnKnownUpdate;
         protected virtual void RaiseKnownUpdate(IDbCommand query)
         {
+            RaiseUnknwonUpdate(query);
             if (!RaiseEvents)
                 return;
             //Async invoke
@@ -81,8 +87,6 @@ namespace JPB.DataAccess.Manager
                 handler.BeginInvoke(this, new DatabaseActionEvent(new QueryDebugger(query)), s => { }, null);
         }
 
-
-        public static event DatabaseActionHandler OnUnknownInsert;
         protected static void RaiseUnknwonInsert(IDbCommand query)
         {
             if (!RaiseStaticEvents)
@@ -93,9 +97,9 @@ namespace JPB.DataAccess.Manager
                 handler.BeginInvoke(null, new DatabaseActionEvent(new QueryDebugger(query)), s => { }, null);
         }
 
-        public event DatabaseActionHandler OnKnownUInsert;
         protected virtual void RaiseKnownInsert(IDbCommand query)
         {
+            RaiseUnknwonInsert(query);
             if (!RaiseEvents)
                 return;
             //Async invoke
