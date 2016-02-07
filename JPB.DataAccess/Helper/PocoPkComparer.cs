2d68d1ef2faf6955ff278422a1ec1b0f150e3274
JPVenson <ger-delta-07@hotmail.de>
2016-02-07 23:46:40 +0100

    Added Licence Headers to all files

--- JPB.DataAccess/Helper/PocoPkComparer.cs
+++ JPB.DataAccess/Helper/PocoPkComparer.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+This work is licensed under the Creative Commons Attribution-ShareAlike 4.0 International License. 
+To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/.
+Please consider to give some Feedback on CodeProject
+
+http://www.codeproject.com/Articles/818690/Yet-Another-ORM-ADO-NET-Wrapper
+
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using JPB.DataAccess.DbInfoConfig;
--- JPB.DataAccess/MetaApi/Model/Equatable/MethodInfoCacheEquatableComparer.cs
+++ JPB.DataAccess/MetaApi/Model/Equatable/MethodInfoCacheEquatableComparer.cs
@@ -1,3 +1,11 @@
+/*
+This work is licensed under the Creative Commons Attribution-ShareAlike 4.0 International License. 
+To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/.
+Please consider to give some Feedback on CodeProject
+
+http://www.codeproject.com/Articles/818690/Yet-Another-ORM-ADO-NET-Wrapper
+
+*/
 using System.Collections.Generic;
 using JPB.DataAccess.MetaApi.Contract;
 
