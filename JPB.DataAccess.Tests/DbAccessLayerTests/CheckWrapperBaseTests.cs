266bea545902393e9cbcf828706068794e8c21e1
Jean-Pierre B. [Home] <ger-delta-07@hotmail.de>
2016-06-07 23:35:25 +0200

    removed duplicated function

--- JPB.DataAccess.Tests/DbAccessLayerTests/CheckWrapperBaseTests.cs
+++ JPB.DataAccess.Tests/DbAccessLayerTests/CheckWrapperBaseTests.cs
@@ -42,14 +42,6 @@ namespace JPB.DataAccess.Tests.DbAccessLayerTests
 			expectWrapper = new Manager().GetWrapper();
 		}
 
-		[SetUp]
-		public void Clear()
-		{
-			expectWrapper.ExecuteGenericCommand(string.Format("DELETE FROM {0} ", UsersMeta.UserTable), null);
-			if (expectWrapper.DbAccessType == DbAccessType.MsSql)
-				expectWrapper.ExecuteGenericCommand(string.Format("TRUNCATE TABLE {0} ", UsersMeta.UserTable), null);
-		}
-
 		[Test]
 		[Category("MsSQL")]
 		[Category("SqLite")]
