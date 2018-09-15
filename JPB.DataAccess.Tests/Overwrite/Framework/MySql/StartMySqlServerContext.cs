fab4f1eb9c85b974ae82fb79d971cee88478c60e
Jean-Pierre B. [Home] <ger-delta-07@hotmail.de>
2018-09-16 00:18:38 +0200

    Fixed NullRefEx in tests when no MySql server was started

--- JPB.DataAccess.Tests/Overwrite/Framework/MySql/StartMySqlServerContext.cs
+++ JPB.DataAccess.Tests/Overwrite/Framework/MySql/StartMySqlServerContext.cs
@@ -17,6 +17,10 @@ public class StartMySqlServerContext
 	[OneTimeTearDown]
 	public void StopMySqlServerEngine()
 	{
+		if (MySqlConnectorInstance.Instance.AwaitMySqlStop == null)
+		{
+			return;
+		}
 		MySqlConnectorInstance.Instance.StopMySql.Set();
 		Assert.That(MySqlConnectorInstance.Instance.AwaitMySqlStop.Wait((int)TimeSpan.FromSeconds(30).TotalMilliseconds), Is.True, "The MySql server did not stop after 30 secounds");
 	}
