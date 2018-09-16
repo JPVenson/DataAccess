388d70ab2821802706bf6e05b11654f7180b60fc
Jean-Pierre B. [Home] <ger-delta-07@hotmail.de>
2018-09-16 18:04:07 +0200

    Added Build script for badge creation

---
+++ NUnitTestsInfo.ps1
@@ -0,0 +1,29 @@
+param($pathToResultsFile)
+
+$xPathToCases = "//UnitTest"
+$pathToResultsFile = Resolve-Path $pathToResultsFile;
+
+if(-not ([System.IO.File]::Exists($pathToResultsFile))){
+    "File Does not exists";
+    $pathToResultsFile
+    exit;
+}
+
+#[xml]$testCasesContent = Get-Content $pathToResultsFile;
+[Reflection.Assembly]::LoadWithPartialName("System.Xml.Linq") | Out-Null
+$doc = [System.Xml.Linq.XDocument]::Load($pathToResultsFile)
+	
+$result = $doc.Root.Elements() | where {$_.Name.LocalName -eq "Results" } | select -First 1
+$tests = $result.Elements() | where {$_.Name.LocalName -eq "UnitTestResult" } 
+$failedTests = $tests | where {$_.Attribute("outcome").Value -eq "Failed"} 
+
+#limit to 10, because otherwise errors are not included in the emailed log
+#foreach ($test in $failedTests | select -First 10) 
+#{
+#	$error = "##vso[task.logissue type=error;]" + $test.Attribute("testName").Value.Trim()
+#	Write-Host $error
+#}
+$countNunitTests = $tests.Length;
+$countFailedNunitTests = $failedTests.Length;
+Write-Host "##vso[task.setvariable variable=Tests.TotalTests]$countNunitTests"
+Write-Host "##vso[task.setvariable variable=Tests.TotalFailedTests]$countFailedNunitTests"
